d8b2620a0773b4355e927e80016b76abb0557fb3
Florian Boor <florian.boor@kernelconcepts.de>
2024-11-14 22:37:09 +0100

    boot_selection.cmd: Rework - unbreak saving parameters. Follow changed variable names.

--- recipes-bsp/rpi-rauc-boot-selection-scr/files/boot_selection.cmd
+++ recipes-bsp/rpi-rauc-boot-selection-scr/files/boot_selection.cmd
@@ -1,10 +1,6 @@
-setenv SAVEENV 0
-test -n "${BOOT_ORDER}" || setenv BOOT_ORDER "A B" && setenv SAVEENV 1
-test -n "${BOOT_A_LEFT}" || setenv BOOT_A_LEFT 3 && setenv SAVEENV 1
-test -n "${BOOT_B_LEFT}" || setenv BOOT_B_LEFT 3 && setenv SAVEENV 1
-if test ${SAVEENV} -gt 0; then
-  saveenv
-fi
+test -n "${BOOT_ORDER}" || setenv BOOT_ORDER "A B"
+test -n "${BOOT_A_LEFT}" || setenv BOOT_A_LEFT 3
+test -n "${BOOT_B_LEFT}" || setenv BOOT_B_LEFT 3
 
 echo "##################################"
 echo "## RAUC UPDATER"
@@ -19,20 +15,20 @@ for BOOT_SLOT in "${BOOT_ORDER}"; do
       setexpr BOOT_A_LEFT ${BOOT_A_LEFT} - 1
       echo "Found valid slot A, ${BOOT_A_LEFT} attempts remaining"
       run default_bootargs
-      setenv bootargs "${bootargs_pi} root=/dev/mmcblk0p2 rauc.slot=A rootwait rootfstype=ext4"
+      setenv bootargs "${bootargs_rpi} root=/dev/mmcblk0p2 rauc.slot=A rootwait rootfstype=ext4"
     fi
   elif test "x${BOOT_SLOT}" = "xB"; then
     if test ${BOOT_B_LEFT} -gt 0; then
       setexpr BOOT_B_LEFT ${BOOT_B_LEFT} - 1
       echo "Found valid slot B, ${BOOT_B_LEFT} attempts remaining"
       run default_bootargs
-      setenv bootargs "${bootargs_pi} root=/dev/mmcblk0p3 rauc.slot=B rootwait rootfstype=ext4"
+      setenv bootargs "${bootargs_rpi} root=/dev/mmcblk0p3 rauc.slot=B rootwait rootfstype=ext4"
     fi
   fi
 done
 
 if test -n "${bootargs}"; then
-#  saveenv
+  saveenv
 else
   echo "No valid slot found, resetting tries to 3"
   setenv BOOT_A_LEFT 3
@@ -40,4 +36,6 @@ else
   saveenv
   reset
 fi
+
+setenv bootargs_rpi "${bootargs}"
 
