72b97f3e28c317cf5abb16fe3dc181f1b315a050
Florian Boor <florian.boor@kernelconcepts.de>
2024-11-25 11:32:18 +0100

    system-update-hook.sh: Copy additional files in RAUC update hook.

--- recipes-bsp/images/files/system-update-hook.sh
+++ recipes-bsp/images/files/system-update-hook.sh
@@ -4,6 +4,13 @@
 DSTDIR=/boot
 SRCDIR=${RAUC_SLOT_MOUNT_POINT}/boot
 
+# copy bootcode blob
+cmp -s ${SRCDIR}/bootcode.bin ${DSTDIR}/bootcode.bin && echo "skip copy bootcode.bin" || cp "${SRCDIR}/bootcode.bin" "${DSTDIR}/bootcode.bin"
+if [ $? -ne 0 ]; then
+	echo Error copying file bootcode.bin
+	exit 2
+fi
+
 # copy bootloader
 cmp -s ${SRCDIR}/u-boot.bin ${DSTDIR}/kernel7.img && echo "skip copy kernel7.img" || cp "${SRCDIR}/u-boot.bin" "${DSTDIR}/kernel7.img"
 if [ $? -ne 0 ]; then
@@ -18,6 +25,13 @@ if [ $? -ne 0 ]; then
 	exit 2
 fi
 
+# copy boot script
+cmp -s ${SRCDIR}/boot.scr ${DSTDIR}/boot.scr && echo "skip copy boot.scr" || cp "${SRCDIR}/boot.scr" "${DSTDIR}/boot.scr"
+if [ $? -ne 0 ]; then
+	echo Error copying file boot.scr
+	exit 2
+fi
+
 # copy kernel image
 cmp -s ${SRCDIR}/uImage ${DSTDIR}/uImage && echo "skip copy uImage" || cp "${SRCDIR}/uImage" "${DSTDIR}/uImage"
 if [ $? -ne 0 ]; then
@@ -27,6 +41,13 @@ fi
 
 # copy configuration
 cmp -s ${SRCDIR}/config.txt ${DSTDIR}/config.txt && echo "skip copy config" || cp "${SRCDIR}/config.txt" "${DSTDIR}/config.txt"
+if [ $? -ne 0 ]; then
+	echo Error copying config
+	exit 2
+fi
+
+# copy configuration
+cmp -s ${SRCDIR}/cmdline.txt ${DSTDIR}/cmdline.txt && echo "skip copy config" || cp "${SRCDIR}/cmdline.txt" "${DSTDIR}/cmdline.txt"
 if [ $? -ne 0 ]; then
 	echo Error copying config
 	exit 2
