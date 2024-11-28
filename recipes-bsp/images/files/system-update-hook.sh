aa9b44236a183a837b246807202a2a23d0bff65f
Christian Hilberg <hilberg@kernelconcepts.de>
2024-11-28 16:24:47 +0100

    system-update-hook.sh: backport all changes made for scarthgap

--- recipes-bsp/images/files/system-update-hook.sh
+++ recipes-bsp/images/files/system-update-hook.sh
@@ -4,6 +4,24 @@
 DSTDIR=/boot
 SRCDIR=${RAUC_SLOT_MOUNT_POINT}/boot
 
+# copy bootcode blob
+cmp -s ${SRCDIR}/bootcode.bin ${DSTDIR}/bootcode.bin && echo "skip copy bootcode.bin" || cp "${SRCDIR}/bootcode.bin" "${DSTDIR}/bootcode.bin"
+if [ $? -ne 0 ]; then
+	echo Error copying file bootcode.bin
+	exit 2
+fi
+
+# copy raspi boot stuff
+for f in $(ls ${SRCDIR}/*.elf ${SRCDIR}/*.dat)
+do
+    f=$(basename ${f})
+    cmp -s "${SRCDIR}/${f}" "${DSTDIR}/${f}" && echo "skip ${f}" || cp "${SRCDIR}/${f}" "${DSTDIR}/${f}"
+    if [ $? -ne 0 ]; then
+        echo Error copying bootstuff ${f}
+        exit 2
+    fi
+done
+
 # copy bootloader
 cmp -s ${SRCDIR}/u-boot.bin ${DSTDIR}/kernel7.img && echo "skip copy kernel7.img" || cp "${SRCDIR}/u-boot.bin" "${DSTDIR}/kernel7.img"
 if [ $? -ne 0 ]; then
@@ -18,6 +36,13 @@ if [ $? -ne 0 ]; then
 	exit 2
 fi
 
+# copy boot script
+cmp -s ${SRCDIR}/boot.scr ${DSTDIR}/boot.scr && echo "skip copy boot.scr" || cp "${SRCDIR}/boot.scr" "${DSTDIR}/boot.scr"
+if [ $? -ne 0 ]; then
+	echo Error copying file boot.scr
+	exit 2
+fi
+
 # copy kernel image
 cmp -s ${SRCDIR}/uImage ${DSTDIR}/uImage && echo "skip copy uImage" || cp "${SRCDIR}/uImage" "${DSTDIR}/uImage"
 if [ $? -ne 0 ]; then
@@ -25,8 +50,49 @@ if [ $? -ne 0 ]; then
 	exit 2
 fi
 
-# copy devicetree / devictree overlays if needed
-# TODO
+# copy configuration
+cmp -s ${SRCDIR}/config.txt ${DSTDIR}/config.txt && echo "skip copy config" || cp "${SRCDIR}/config.txt" "${DSTDIR}/config.txt"
+if [ $? -ne 0 ]; then
+	echo Error copying config
+	exit 2
+fi
 
+# copy kernel cmdline
+cmp -s ${SRCDIR}/cmdline.txt ${DSTDIR}/cmdline.txt && echo "skip copy kernel cmdline" || cp "${SRCDIR}/cmdline.txt" "${DSTDIR}/cmdline.txt"
+if [ $? -ne 0 ]; then
+	echo Error copying kernel cmdline
+	exit 2
+fi
+
+# copy devicetrees
+for f in $(ls ${SRCDIR}/*.dtb)
+do
+    f=$(basename ${f})
+    cmp -s "${SRCDIR}/${f}" "${DSTDIR}/${f}" && echo "skip ${f}" || cp "${SRCDIR}/${f}" "${DSTDIR}/${f}"
+    if [ $? -ne 0 ]; then
+        echo Error copying devicetree ${f}
+        exit 2
+    fi
+done
+
+# copy devicetree overlays
+for f in $(ls ${SRCDIR}/*.dtbo)
+do
+    f=$(basename ${f})
+    cmp -s "${SRCDIR}/${f}" "${DSTDIR}/overlays/${f}" && echo "skip ${f}" || cp "${SRCDIR}/${f}" "${DSTDIR}/overlays/${f}"
+    if [ $? -ne 0 ]; then
+        echo Error copying overlay ${f}
+        exit 2
+    fi
+done
+
+# copy overlay map
+cmp -s "${SRCDIR}/overlay_map.dtb" "${DSTDIR}/overlays/overlay_map.dtb" && echo "skip overlay_map.dtb" || cp "${SRCDIR}/overlay_map.dtb" "${DSTDIR}/overlays/overlay_map.dtb"
+if [ $? -ne 0 ]; then
+    echo Error copying overlay map
+    exit 2
+fi
 
 sync
+
+exit 0
